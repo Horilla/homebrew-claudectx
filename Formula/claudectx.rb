35b9cf198dda2d1bdc0ae9e5723f17d40666ab88
Horilla <support@horilla.com>
2026-04-12 13:33:33 +0530

    [UPDT] CLAUDECTX: Bump formula to v1.1.3 (safety & backup system)

--- Formula/claudectx.rb
+++ Formula/claudectx.rb
@@ -1,8 +1,8 @@
 class Claudectx < Formula
   desc "Reduce Claude Code token usage by up to 80%"
   homepage "https://github.com/Horilla/claudectx"
-  url "https://registry.npmjs.org/claudectx/-/claudectx-1.1.2.tgz"
-  sha256 "4bc23dece5aee4493f3ac7b7cf3884d41f9279ac57dddea357b2625023eae19a"
+  url "https://registry.npmjs.org/claudectx/-/claudectx-1.1.3.tgz"
+  sha256 "9e1eae59894bd0325cc86a440d42ba61fe9db8ad7caa37453a085fde55839412"
   license "MIT"
 
   depends_on "node"
@@ -13,6 +13,6 @@ class Claudectx < Formula
   end
 
   test do
-    assert_match "1.1.2", shell_output("#{bin}/claudectx --version")
+    assert_match "1.1.3", shell_output("#{bin}/claudectx --version")
   end
 end
